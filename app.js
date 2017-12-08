3fee88cdfa095c97e420c79a67dd411b5fae3f2a
Nikhil Bang <nbang0506@gmail.com>
2017-12-08 22:36:18 +0530

    Initail commit

---
+++ app.js
@@ -0,0 +1,7 @@
+var myApp = angular.module("myModule", []);
+
+// Register controller with the module
+myApp.controller("myController", function ($scope) {
+    $scope.message = "AngularJS Tutorial";
+});
+
