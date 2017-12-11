e4e0a821b8acd59b3c13e2d06a1cfd2764b9e2ef
Nikhil Bang <nbang0506@gmail.com>
2017-12-11 20:27:11 +0530

    sort rows by table header

--- demo.js
+++ demo.js
@@ -1,129 +1,46 @@
+var app = angular
+        .module("myModule", [])
+        .controller("myController", function ($scope) {
 
-//# first way
-//var myApp = angular.module("myModule", []);
+            var employees = [
+                {
+                    name: "ABC", dateOfBirth: new Date("November 23, 1992"),
+                    gender: "Male", salary: 20050
+                },
+                {
+                    name: "CCC", dateOfBirth: new Date("May 05, 1992"),
+                    gender: "Female", salary: 20050
+                },
+                {
+                    name: "BBB", dateOfBirth: new Date("August 15, 1992"),
+                    gender: "Male", salary: 20050
+                },
+                {
+                    name: "DDD", dateOfBirth: new Date("October 27, 1992"),
+                    gender: "Female", salary: 53000
+                },
+                {
+                    name: "FFF", dateOfBirth: new Date("December 30, 1992"),
+                    gender: "Male", salary: 20050
+                }
+            ];
 
-/*
-// Creating the controller and registering with the module all done in one line using ES6
-myApp.controller("myController",($scope) => {
-	$scope.message = "AngularJS test app";
-	var emp =  {
-         firstName: 'Kapil',
-         lastName: 'Bang',
-         gender: 'Male'
-    };
+            $scope.employees = employees;
+            $scope.sortColumn = "name";
+            $scope.reverseSort = false;
 
-    $scope.emp = emp;
-});
-*/
+            $scope.sortData = function (column) {
+                $scope.reverseSort = ($scope.sortColumn == column) ?
+                    !$scope.reverseSort : false;
+                $scope.sortColumn = column;
+            }
 
-//# second way
+            $scope.getSortClass = function (column) {
 
-/*
-	//Create the controller
-	var myController = function ($scope) {
-	    $scope.message = "AngularJS test app";
-	}
-
-
-	// Register the controller with the module
-	myApp.controller("myController", myController);
-
-	*/
-
-
-//# threed way
-
-//# Creating the controller and registering with the module all done in one line using ES5
-
-/*
-myApp.controller("mycontroller", function ($scope) {
-	    $scope.message = "AngularJS test app";
-})
-
-*/
-
-// fourth way method chaining mechanism
-
-var myApp = angular
-.module("myModule", [])
-.controller("myController",($scope) => {
-	$scope.message = "AngularJS test app";
-
-	var emp = [
-	{
-		name: "aa", dateOfBirth: new Date("November 23, 2012"),
-		gender: "Male", salary: 55000.788
-	},
-	{
-		name: "bb", dateOfBirth: new Date("May 05, 2012"),
-		gender: "Female", salary: 68000
-	},
-	{
-		name: "cc", dateOfBirth: new Date("August 15, 2012"),
-		gender: "Male", salary: 57000
-	},
-	{
-		name: "dd", dateOfBirth: new Date("October 27, 2012"),
-		gender: "Female", salary: 53000
-	},
-	{
-		name: "ddff", dateOfBirth: new Date("December 30, 2012"),
-		gender: "Male", salary: 60000
-	}
-	];
-
-
-
-	$scope.emp = emp;
-	$scope.rowCount = 2;
-
-	var countries = [
-	{
-		name: "UK",
-		cities: [
-		{ name: "London" },
-		{ name: "Birmingham" },
-		{ name: "Manchester" }
-		]
-	},
-	{
-		name: "USA",
-		cities: [
-		{ name: "Los Angeles" },
-		{ name: "Chicago" },
-		{ name: "Houston" }
-		]
-	},
-	{
-		name: "India",
-		cities: [
-		{ name: "Hyderabad" },
-		{ name: "Chennai" },
-		{ name: "Mumbai" }
-		]
-	}
-	];
-
-	$scope.countries = countries;
-
-
-
-	var technologies = [
-	{ name: "Ruby", likes: 0, dislikes: 0 },
-	{ name: "Node js", likes: 0, dislikes: 0 },
-	{ name: "PHP", likes: 0, dislikes: 0 },
-	{ name: "AngularJS", likes: 0, dislikes: 0 }
-	];
-
-	$scope.technologies = technologies;
-
-	$scope.incrementLikes = function (technology) {
-		technology.likes++;
-	};
-
-	$scope.incrementDislikes = function (technology) {
-		technology.dislikes++;
-	};
-});
-
-
+                if ($scope.sortColumn == column) {
+                    return $scope.reverseSort ? 'arrow-down' : 'arrow-up';
+                }
+20050
+                return '';
+            }
+        });
