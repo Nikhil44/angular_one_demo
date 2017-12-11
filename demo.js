d8d9dc491a84df6ddff3690f1ff7e81fab29e5f9
Nikhil Bang <nbang0506@gmail.com>
2017-12-11 21:15:12 +0530

    refactor custom filter

--- demo.js
+++ demo.js
@@ -1,24 +1,15 @@
+
 var app = angular
         .module("myModule", [])
-        .filter("gender", function () {
-            return function (gender) {
-                switch (gender) {
-                    case 1:
-                        return "Male";
-                    case 2:
-                        return "Female";
-                    case 3:
-                        return "Not disclosed";
-                }
-            }
-        })
         .controller("myController", function ($scope) {
+
             var employees = [
-                { name: "Abcd", gender: 1, salary: 55000 },
-                { name: "Pqrs", gender: 2, salary: 68000 },
-                { name: "Xyze", gender: 1, salary: 57000 },
-                { name: "tttt", gender: 2, salary: 53000 },
-                { name: "Abcd", gender: 3, salary: 60000 }
+                { name: "Ben", gender: 1, salary: 55000 },
+                { name: "Sara", gender: 2, salary: 68000 },
+                { name: "Mark", gender: 1, salary: 57000 },
+                { name: "Pam", gender: 2, salary: 53000 },
+                { name: "Todd", gender: 3, salary: 60000 }
             ];
+
             $scope.employees = employees;
         });
