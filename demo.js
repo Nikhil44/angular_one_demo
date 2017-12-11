7a746c574f11e94beeafdf53424b4bc42a4727c2
Nikhil Bang <nbang0506@gmail.com>
2017-12-11 16:41:27 +0530

    method chaining mechanism

--- demo.js
+++ demo.js
@@ -1,12 +1,23 @@
-var myApp = angular.module("myModule", []);
 
 //# first way
+//var myApp = angular.module("myModule", []);
 
-
+/*
 // Creating the controller and registering with the module all done in one line using ES6
-myApp.controller("myController",($scope) => {$scope.message = "AngularJS test app";});
+myApp.controller("myController",($scope) => {
+	$scope.message = "AngularJS test app";
+	var emp =  {
+         firstName: 'Kapil',
+         lastName: 'Bang',
+         gender: 'Male'
+    };
+
+    $scope.emp = emp;
+});
+*/
 
 //# second way
+
 /*
 	//Create the controller
 	var myController = function ($scope) {
@@ -20,10 +31,29 @@ myApp.controller("myController",($scope) => {$scope.message = "AngularJS test ap
  */
 
 
+//# threed way
+
 //# Creating the controller and registering with the module all done in one line using ES5
+
 /*
 myApp.controller("mycontroller", function ($scope) {
 	    $scope.message = "AngularJS test app";
 })
 
  */
+
+// fourth way method chaining mechanism
+
+var myApp = angular
+    .module("myModule", [])
+    .controller("myController",($scope) => {
+    	$scope.message = "AngularJS test app";
+        var emp = {
+            firstName: 'Kapil',
+            lastName: 'Bang',
+            gender: 'Male'
+        };
+        $scope.emp = emp;
+    });
+
+
