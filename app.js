0d003045d8f3c8efb9f5458899d3a1b1a132d36d
Nikhil Bang <nbang0506@gmail.com>
2017-12-11 16:21:19 +0530

    initail setup

--- app.js
+++
@@ -1,7 +0,0 @@
-var myApp = angular.module("myModule", []);
-
-// Register controller with the module
-myApp.controller("myController", function ($scope) {
-    $scope.message = "AngularJS Tutorial";
-});
-
