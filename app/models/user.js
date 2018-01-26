7b539b3bce1403ae5ab1bc3cec1f7eacf95ba21c
Nikhil44 <nbang0506@gmail.com>
2018-01-26 16:27:32 +0530

    backend validation

--- app/models/user.js
+++ app/models/user.js
@@ -1,17 +1,62 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var bcrypt = require('bcrypt-nodejs');
+var titlize = require('mongoose-title-case'); // Import Mongoose Title Case Plugin
+var validate = require('mongoose-validator'); // Import Mongoose Validator Plugin
 
+// User E-mail Validator
+var emailValidator = [
+  validate({
+    validator: 'matches',
+    arguments: /^[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/,
+    message: 'Name must be at least 3 characters, max 40, no special characters or numbers, must have space in between name.'
+  }),
+  validate({
+    validator: 'isLength',
+    arguments: [3, 40],
+    message: 'Email should be between {ARGS[0]} and {ARGS[1]} characters'
+  })
+];
+
+// Username Validator
+var usernameValidator = [
+  validate({
+    validator: 'isLength',
+    arguments: [3, 25],
+    message: 'Username should be between {ARGS[0]} and {ARGS[1]} characters'
+  }),
+  validate({
+    validator: 'isAlphanumeric',
+    message: 'Username must contain letters and numbers only'
+  })
+];
+
+// Password Validator
+var passwordValidator = [
+  validate({
+    validator: 'matches',
+    arguments: /^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])(?=.*?[\W]).{8,35}$/,
+    message: 'Password needs to have at least one lower case, one uppercase, one number, one special character, and must be at least 8 characters but no more than 35.'
+  }),
+  validate({
+    validator: 'isLength',
+    arguments: [8, 35],
+    message: 'Password should be between {ARGS[0]} and {ARGS[1]} characters'
+  })
+];
+
+// User Mongoose Schema
 var UserSchema = new Schema({
-  firstname: {type: String, lowercase: true, required: true },
-  lastname:  {type: String, lowercase: true, required: true },
-  username:  {type: String, lowercase: true, required: true, unique: true },
-  password:  {type: String, required: true},
-  email: {type: String, required: true, lowercase: true, unique: true}
+  firstname: {type: String, required: true },
+  lastname:  {type: String, required: true },
+  username: { type: String, lowercase: true, required: true, unique: true, validate: usernameValidator },
+  password: { type: String, required: true, validate: passwordValidator, select: false },
+  email: { type: String, required: true, lowercase: true, unique: true, validate: emailValidator }
 });
 
 UserSchema.pre('save', function(next) {
   var user = this;
+  if (!user.isModified('password')) return next(); 
   bcrypt.hash(user.password,null,null, function (err, hash) {
     if(err)
       return next(err);
@@ -24,7 +69,8 @@ UserSchema.methods.comparePassword = function (password) {
   return bcrypt.compareSync(password,this.password);
 };
 
-
-
+UserSchema.plugin(titlize, {
+  paths: ['firstname','lastname']
+});
 
 module.exports = mongoose.model('User', UserSchema);
--- app/routes/api.js
+++ app/routes/api.js
@@ -6,18 +6,46 @@ module.exports = function (router) {
 // user reegistration
   router.post('/users', function (req,res) {
       var user = new User();
+      
       user.firstname = req.body.firstname;
       user.lastname = req.body.lastname;
       user.username = req.body.username;
       user.password = req.body.password;
       user.email = req.body.email;
-      if(req.body.username == null || req.body.password == null || req.body.email == '' || req.body.email == null){
+    console.log(user);
+    if (req.body.firstname == '' || req.body.firstname == null || req.body.lastname == '' || req.body.lastname == null || req.body.username == null || req.body.username == '' || req.body.password == null || req.body.password == '' || req.body.email == '' || req.body.email == null){
           res.json({success: false, message: 'Ensure username email and password'});
       }
       else {
         user.save(function(err) {
-          if (err){
-            res.json({success: false, message: 'User name or email is already exists'});
+          if (err) {
+            if (err.errors !== null) {
+              console.log(err)
+              if (err.errors.firstname) {
+                res.json({ success: false, message: err.errors.firstname.message }); 
+              }
+              else if (err.errors.lastname) {
+                res.json({ success: false, message: err.errors.lastname.message }); 
+              } else if (err.errors.email) {
+                res.json({ success: false, message: err.errors.email.message }); 
+              } else if (err.errors.username) {
+                res.json({ success: false, message: err.errors.username.message });
+              } else if (err.errors.password) {
+                res.json({ success: false, message: err.errors.password.message }); 
+              } else {
+                res.json({ success: false, message: err });
+              }
+            } else if (err) {
+              if (err.code == 11000) {
+                if (err.errmsg[61] == "u") {
+                  res.json({ success: false, message: 'That username is already taken' }); 
+                } else if (err.errmsg[61] == "e") {
+                  res.json({ success: false, message: 'That e-mail is already taken' });
+                }
+              } else {
+                res.json({ success: false, message: err })
+              }
+            }
           }
           else {
             res.json({success: true, message: 'user created'});
@@ -25,7 +53,7 @@ module.exports = function (router) {
         });
       }
   });
-  // user login route
+ 
   router.post('/authenticate', function(req, res){
       User.findOne({ username: req.body.username },'email username password').exec(function(err,user){
         if(!user)
